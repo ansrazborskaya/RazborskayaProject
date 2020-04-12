15f93f9f4eb62716631e2dfdf0356ca5413de914
ansrazborskaya <ans.razborskaya@gmail.com>
2020-04-12 19:14:39 +0300

    test

---
+++ src/homework/Task2.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        String s = "\t It's Windows path: \"C:\\Program \n Files\\Java\\jdk1.8.0\\bin\"";
+        System.out.println(s);
+
+
+    }
+}
